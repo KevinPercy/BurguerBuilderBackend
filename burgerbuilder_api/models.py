ac74a3feebeedd1695a1aeeb1285e2340de8c4dc
KevinPercy <kevinpercycl@gmail.com>
2020-05-19 09:44:16 -0500

    Added custom user profile manager and migrations

--- burgerbuilder_api/models.py
+++ burgerbuilder_api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    
+    def create_user(self, email, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email) # takes care of the second half     
+        user =  self.model(email=email)   
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser with given details """
+        user = self.create_user(email, password)
+
+        user.is_superuser = True
+        
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique= True)
+    is_active = models.BooleanField(default= True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    
+    def __str__(self):
+        """Return the string representation of our user"""
+        return self.email
